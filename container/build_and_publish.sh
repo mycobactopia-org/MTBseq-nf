574603e552f80f895f7f5050af10fc1c6923d14e
Abhinav Sharma <abhi18av@users.noreply.github.com>
2022-10-01 15:11:41 +0200

    Bundle the gatk jar in the container (#75)
    * Update Dockerfile
    
    * update the container build script
    
    * update tag
    
    * Update nextflow.config
    
    * explain docker setup
    
    * restructure contribution order

--- container/build_and_publish.sh
+++ container/build_and_publish.sh
@@ -4,15 +4,15 @@ set -uex
 # NOTE: Make sure you've set the environment correctly and are logged in to the registry.
 # NOTE: Login to github registry with `echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin`
 
-CONTAINER_TAG=0.9.6
+CONTAINER_TAG=0.9.7
 
 DOCKER_NAMESPACE="ghcr.io/mtb-bioinformatics"
 
 # NOTE: Do not embed the GATK jar inside the container since at process-level we already do gatk-register
-# echo "Downloading and uncompressing GATK jar"
-# wget "https://storage.googleapis.com/gatk-software/package-archive/gatk/GenomeAnalysisTK-3.8-0-ge9d806836.tar.bz2"
-# tar -xf GenomeAnalysisTK-3.8-0-ge9d806836.tar.bz2 --wildcards '*.jar'
-# cp GenomeAnalysis*/*.jar .
+echo "Downloading and uncompressing GATK jar"
+wget "https://storage.googleapis.com/gatk-software/package-archive/gatk/GenomeAnalysisTK-3.8-0-ge9d806836.tar.bz2"
+tar -xf GenomeAnalysisTK-3.8-0-ge9d806836.tar.bz2 --wildcards '*.jar'
+cp GenomeAnalysis*/*.jar .
 
 echo "Copying the mtbseq-nf-env file"
 cp ../conda_envs/mtbseq-nf-env.yml ./
