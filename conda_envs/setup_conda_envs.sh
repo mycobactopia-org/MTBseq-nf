5d908efc362068c67a55f97bc3492dc451b1e05d
Abhinav Sharma <abhi18av@gmail.com>
2022-10-01 17:01:39 +0200

    remove extra profiles

--- conda_envs/setup_conda_envs.sh
+++ conda_envs/setup_conda_envs.sh
@@ -4,9 +4,11 @@ set -e
 
 # NOTE: Please replace `conda` with `mamba` if it is installed for faster installs.
 
-# NOTE: The conda environments are expected by the `conda_local` profile to be created within `conda_envs` directory
+condaBinary="conda"
 
-# NOTE: Adding a step to automatically register the gatk jar
+#NOTE: The conda environments are expected by the `conda_local` profile to be created within `conda_envs` directory
+
+#NOTE: Adding a step to automatically register the gatk jar
 echo "Downloading GATK jar"
 
 wget "https://storage.googleapis.com/gatk-software/package-archive/gatk/GenomeAnalysisTK-3.8-0-ge9d806836.tar.bz2"
@@ -16,18 +18,22 @@ tar -xf GenomeAnalysisTK-3.8-0-ge9d806836.tar.bz2 --wildcards '*.jar'
 cp GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar .
 
 echo "Creating mtbseq-nf-env"
-mamba env create -p mtbseq-nf-env --file mtbseq-nf-env.yml
+$condaBinary env create -p mtbseq-nf-env --file mtbseq-nf-env.yml
 
 echo "Registering GATK Jar"
 
 # TODO: Good candidate for a clean approach in a refactor.
 eval "$(conda shell.bash hook)"
-conda activate "./mtbseq-nf-env"
+$condaBinary activate "./mtbseq-nf-env"
+
 gatk-register GenomeAnalysisTK.jar 
 
 echo "Testing mtbseq-nf-env"
 MTBseq --version
 MTBseq --check
+
+echo "Cleaning files"
+rm -rf GenomeAnalysisTK*
 
 echo "Cleaning files"
 rm -rf GenomeAnalysisTK*
